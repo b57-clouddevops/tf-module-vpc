572415fe50f615aa7d6eb3081eb60e5b73091431
CodingManoj <CodingManoj@gmail.com>
2024-04-23 21:13:09 -0400

    Auto Push

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,10 @@
 # Creates Public Subnets
 resource "aws_subnet" "public_subnet" {
-  count             = length(var.PUBLIC_SUBNET_CIDR)
+  count                     = length(var.PUBLIC_SUBNET_CIDR)
 
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.PUBLIC_SUBNET_CIDR, count.index) 
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = element(var.PUBLIC_SUBNET_CIDR, count.index) 
+  map_public_ip_on_launch   = true 
 
   tags = {
     Name = "roboshop-${var.ENV}-public-subnet-${count.index}"
@@ -13,10 +14,10 @@ resource "aws_subnet" "public_subnet" {
 
 # Creates Private Subnets
 resource "aws_subnet" "private_subnet" {
-  count             = length(var.PRIVATE_SUBNET_CIDR)
+  count                     = length(var.PRIVATE_SUBNET_CIDR)
 
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.PRIVATE_SUBNET_CIDR, count.index) 
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = element(var.PRIVATE_SUBNET_CIDR, count.index) 
 
   tags = {
     Name = "roboshop-${var.ENV}-private-subnet-${count.index}"
