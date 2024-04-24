df538465a6810417dc4fae142e2b3fe30c5be5d5
CodingManoj <CodingManoj@gmail.com>
2024-04-23 21:22:57 -0400

    Auto Push

---
+++ routeTable.tf
@@ -0,0 +1,25 @@
+# Creates Public Route Table 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "roboshop-${var.ENV}-public-rt"
+  }
+}
+
+# Creates Private Route Table 
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = var.VPC_CIDR
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+  tags = {
+    Name = "roboshop-${var.ENV}-private-rt"
+  }
+}
