82b1600aa2ee8ea504a6af41050948763f652d1b
CodingManoj <CodingManoj@gmail.com>
2024-04-23 21:48:55 -0400

    Auto Push

--- routeTable.tf
+++ routeTable.tf
@@ -23,10 +23,10 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
-#   route {
-#     cidr_block = var.VPC_CIDR
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.ngw.id 
+  }
   tags = {
     Name = "roboshop-${var.ENV}-private-rt"
   }
