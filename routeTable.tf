12256716eb7009cf9c3f4f40a930df27e800fa64
CodingManoj <CodingManoj@gmail.com>
2024-04-23 21:56:09 -0400

    Auto Push

--- routeTable.tf
+++ routeTable.tf
@@ -9,7 +9,7 @@ resource "aws_route_table" "public_rt" {
 
   route {
     cidr_block = var.DEFAULT_VPC_CIDR
-    gateway_id = aws_internet_gateway.igw.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id 
   }
 
   tags = {
@@ -32,6 +32,11 @@ resource "aws_route_table" "private_rt" {
   route {
     cidr_block      = "0.0.0.0/0"
     nat_gateway_id  = aws_nat_gateway.ngw.id 
+  }
+
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id 
   }
   tags = {
     Name = "roboshop-${var.ENV}-private-rt"
