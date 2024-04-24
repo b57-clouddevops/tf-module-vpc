1e45945416d493a65e812bfce911aad50e27cb16
CodingManoj <CodingManoj@gmail.com>
2024-04-23 20:51:31 -0400

    Auto Push

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "main" {
   cidr_block            = var.VPC_CIDR
   instance_tenancy      = "default"
-
+  enable_dns_hostnames  = true 
 
   tags = {
     Name = "roboshop-${var.ENV}-vpc"
