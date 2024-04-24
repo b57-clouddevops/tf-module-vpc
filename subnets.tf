4e0442e7e86a8ff5ae5ad05be379e9146ec80647
CodingManoj <CodingManoj@gmail.com>
2024-04-23 21:07:48 -0400

    Auto Push

---
+++ subnets.tf
@@ -0,0 +1,24 @@
+# Creates Public Subnets
+resource "aws_subnet" "public_subnet" {
+  count             = length(var.PUBLIC_SUBNET_CIDR)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.PUBLIC_SUBNET_CIDR, count.index) 
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-subnet-${count.index}"
+  }
+}
+
+
+# Creates Private Subnets
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.PRIVATE_SUBNET_CIDR)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.PRIVATE_SUBNET_CIDR, count.index) 
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-subnet-${count.index}"
+  }
+}
